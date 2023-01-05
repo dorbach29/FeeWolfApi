612069bcd2ef7f6b0433b9a9c06f0e4023b3bcae
Tommybones2 <tbonagu1@gmail.com>
2023-01-04 22:56:49 -0500

    Revamp of Exchange Files. Need help with exchange.js (route)

--- Exchange/ExchangeLogic.js
+++ Exchange/ExchangeLogic.js
@@ -1,53 +1,100 @@
 const axios = require('axios');
 require('dotenv').config();
 
-//const exchangeNameList = ["Coinbase", "Gemini", "Kucoin", "Binance", "CryptoCom", "FTX"];
 let amount;
 let marketOrder = true;
+//METADATA
+const ExchangeMetaData = {
+    Exchanges : {
+        "Coinbase" : {
+            logoLink : "/Coinbase.png",
+            logoDesc : "Coinbase Logo",
+            name : "Coinbase",
+            withdrawlFee : -1,
+            amount : 0,
+        },
+        "Gemini" :  {
+            logoLink : "/Gemini.png",
+            logoDesc : "Gemini Logo",
+            name : "Gemini",
+            withdrawlFee : -1,
+            amount : 0,
+          },
+        "Kucoin" : {
+            logoLink : "/Kucoin.png",
+            logoDesc : "Kucoin Logo",
+            name: "Kucoin",
+            withdrawlFee : -1,
+            amount : 0,
+        },
+        "Binance" : {
+            logoLink : "/Binance.png",
+            logoDesc : "Binance Logo",
+            name: "Binance",
+            withdrawlFee : -1,
+            amount : 0,
+        },
+        "CryptoCom" : {
+            logoLink : "/CryptoCom.png",
+            logoDesc : "CryptoCom Logo",
+            name: "CryptoCom",
+            withdrawlFee : -1,
+            amount: 0,
+        },
+        "FTX" : {
+            logoLink : "/FTX.png",
+            logoDesc : "FTX Logo",
+            name: "FTX",
+            withdrawlFee : -1,
+            amount : 0,
+        },
+        
+        
+    }
+};
+//Exchange Methods
 // True = MarketOrder (maker), False = Limit Order (taker)
 //Maker = market Order
 //Taker = limit order
 function CoinbaseFee(amount){
-    let coinFee = {
-        transactionFee : -1,
-    }
+    let coinFee = 0;
     if(amount < 10){
-        coinFee.transactionFee = .99;
+        coinFee = .99;
     } else if(amount < 25){
-        coinFee.transactionFee = 1.49;
+        coinFee = 1.49;
     } else if(amount < 50){
-        coinFee.transactionFee = 1.99;
+        coinFee = 1.99;
     } else if (amount < 200){
-        coinFee.transactionFee = 2.99;
+        coinFee = 2.99;
     }
-    return coinFee;
+    return amount - coinFee;
     //it would be return amount + coinFee if we want the actual whole cost.
 }
 
 function GeminiFee(amount){
-    let gemFee= {
-        transactionFee : -1,
-    }
+    let gemFee = 0;
     //This is specific to USD obviously.
-    if(amount <= 10.00){
-        gemFee.transactionFee = 0.99;
+    if(amount <= 10){
+        gemFee = 0.99;
     } 
-    else if ((amount > 10.00)&&(amount <= 25.00)){
-        gemFee.transactionFee = 1.49;
+    else if ((amount > 10)&&(amount <= 25)){
+        gemFee = 1.49;
     }
-    else if ((amount > 25.00)&&(amount <= 50.00)){
-        gemFee.transactionFee = 1.99;
+    else if ((amount > 25)&&(amount <= 50)){
+        gemFee = 1.99;
     }
-    else if ((amount > 50.00)&&(amount <= 200.00)){
-        gemFee.transactionFee = 2.99;
+    else if ((amount > 50)&&(amount < 200)){
+        gemFee = 2.99;
     }
-    else if(amount >200.00){
+    else if (amount >= 200){
         //Above 200 is 1.49%
-        gemFee.transactionFee = amount * .0149;
+        gemFee = amount * .0149;
     }
-    return gemFee;
+    //return gemFee;
+    return amount - gemFee;
     //return gemFee + amount
 }
+
 
 function KuCoinFee(amount, marketOrder){
     let kuFee = 0.0;
@@ -131,14 +178,14 @@ function KuCoinFee(amount, marketOrder){
             kuFee = amount * .0003;
         }
     }
-    return kuFee;
+    return amount - kuFee;
     //return kuFee + amount
 }
 
 function BinanceFee(amount){
     let biFee = 0.0;
     biFee = amount * .0010;
-    return biFee;
+    return amount - biFee;
 }
 
 function CryptoComFee(amount, marketOrder){
@@ -196,8 +243,8 @@ function CryptoComFee(amount, marketOrder){
             comFee = amount * .0012;
         }
     }
-    return comFee;
-    //return kuFee + amount
+    return amount - comFee;
+   
 }
 
 function FTXFee(amount, marketOrder){
@@ -219,8 +266,8 @@ function FTXFee(amount, marketOrder){
         else if((amount >= 25000000)&&(amount < 50000000)){
              ftxFee = amount;
         }
-        else if((amount > 50000000)){
-            ftxFee = amount;
+        else if((amount >= 50000000)){
+            ftxFee = 0;
         }
     }
     if (marketOrder == false){
@@ -239,11 +286,103 @@ function FTXFee(amount, marketOrder){
         else if((amount >= 25000000)&&(amount < 50000000)){
              ftxFee = amount * .0045;
         }
-        else if((amount > 50000000)){
+        else if((amount >= 50000000)){
             ftxFee = amount * .0040;
         }
     }
-    return ftxFee;
+    return amount - ftxFee;
 
 }
 
+function getExchangeData(exchangeName, transactionFees) {
+  const exchangeData = ExchangeMetaData.Exchanges[exchangeName];
+  if (exchangeData) {
+    return {
+      ...exchangeData,
+      fee: transactionFees[exchangeName]
+    };
+  } else {
+    return null;
+  }
+}
+
+function getAllTransactions(amount, marketOrder) {
+  const transactionFees = {
+    Coinbase: CoinbaseFee(amount),
+    Gemini: GeminiFee(amount),
+    Kucoin: KuCoinFee(amount, marketOrder),
+    Binance: BinanceFee(amount, marketOrder),
+    CryptoCom: CryptoComFee(amount),
+    FTX: FTXFee(amount, marketOrder),
+  };
+
+  // Add the calculated fees to the metadata for each exchange
+  Object.keys(transactionFees).forEach((exchange) => {
+    ExchangeMetaData.Exchanges[exchange].amount = transactionFees[exchange];
+  });
+
+  function search(name) {
+    return getExchangeData(name, transactionFees);
+  }
+
+  return {
+    transactionFees,
+    search,
+  };
+}
+
+const data = getAllTransactions(50, true);
+console.log(data.transactionFees); // { Coinbase: 48.51, Gemini: 48.01, Kucoin: 49.5, Binance: 49.5, "CryptoCom": 50, FTX: 49.5 }
+/*
+const exchangeData = data.search("Coinbase");
+console.log(exchangeData); // { logoLink: '/Coinbase.png', logoDesc: 'Coinbase Logo', name: 'Coinbase', withdrawlFee: -1, transactionFee: 0, fee: 48.51 }
+*/
+
+// Define the function to sort the exchanges based on their calculated fees
+// Define the function to sort the exchanges based on their calculated fees
+// Define the function to sort the exchanges based on their calculated fees
+function sortTransactions(metadata) {
+  // Create a copy of the metadata object
+  const sortedMetadata = { ...metadata };
+  // Create an array of the metadata for each exchange
+  const exchanges = Object.values(sortedMetadata.Exchanges);
+  // Sort the array of exchanges based on the calculated fee
+  const sortedExchanges = exchanges.sort((a, b) => {
+    // Compare the fees for each exchange
+    if (a.amount > b.amount) {
+      return -1;
+    }
+    if (a.amount < b.amount) {
+      return 1;
+    }
+    // If the fees are equal, return 0
+    return 0;
+  });
+  // Update the sorted metadata object with the sorted exchanges
+  sortedMetadata.Exchanges = sortedExchanges;
+  // Return the sorted metadata object
+  return sortedMetadata;
+}
+
+// Define the function to print all of the metadata
+function printAllMetadata(metadata) {
+  // Iterate through the metadata for each exchange
+  Object.values(metadata.Exchanges).forEach((exchange) => {
+    // Print the metadata for each exchange
+    console.log(`Exchange: ${exchange.name}`);
+    console.log(`Logo Link: ${exchange.logoLink}`);
+    console.log(`Logo Description: ${exchange.logoDesc}`);
+    console.log(`Withdrawl Fee: ${exchange.withdrawlFee}`);
+    console.log(`Amount: ${exchange.amount}`);
+    console.log("------------------------------");
+  });
+}
+// Sort the exchanges based on their calculated fees
+//sortTransactions(ExchangeMetaData);
+// Print all of the metadata
+//printAllMetadata(ExchangeMetaData);
+console.log("SORTED NOW");
+// Sort the metadata object and store a copy of the sorted object
+const sortedMetadata = sortTransactions(ExchangeMetaData);
+// Print all of the metadata
+printAllMetadata(sortedMetadata);
--- Routes/exchange.js
+++ Routes/exchange.js
@@ -1,86 +1,65 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-//Some taken from ChatGPT
+const express = require("express");
+const router = express.Router();
 
-// Import ExchangeHandler module
-const ExchangeHandler = require('./ExchangeHandler');
+// Import the exchange data and functions
+const ExchangeMetaData = require("./path/to/exchange/data");
+const CoinbaseFee = require("./path/to/exchange/data").CoinbaseFee;
+const GeminiFee = require("./path/to/exchange/data").GeminiFee;
+const KuCoinFee = require("./path/to/exchange/data").KuCoinFee;
+const BinanceFee = require("./path/to/exchange/data").BinanceFee;
+const CryptoComFee = require("./path/to/exchange/data").CryptoComFee;
+const FTXFee = require("./path/to/exchange/data").FTXFee;
 
-// Set up route to get exchange data
-app.get('/exchange/:name', (req, res) => {
-  // Get exchange name from URL parameter
-  const exchangeName = req.params.name;
-  // Get exchange data
-  const exchangeData = ExchangeHandler.getExchangeData(exchangeName);
-  // Send exchange data as response
-  res.send(exchangeData);
+/**
+ * "/exchanges"
+ * 
+ * Returns the entire ExchangeMetaData object.
+ */
+router.get("/", (req, res, next) => {
+  res.json(ExchangeMetaData);
 });
 
-// Set up route to set new Coinbase fee
-app.put('/exchange/coinbase/:fee', (req, res) => {
-  // Get fee from URL parameter
-  const fee = req.params.fee;
-  // Set new Coinbase fee
-  ExchangeHandler.setNewCoinbaseFee(fee);
-  // Send success message as response
-  res.send(`Successfully set Coinbase fee to ${fee}`);
+/**
+ * "/exchanges/:exchange/fees"
+ * 
+ * Returns the fee for a trade on the specified exchange.
+ * 
+ * @param {string} exchange - The name of the exchange.
+ */
+router.get("/:exchange/fees", (req, res, next) => {
+  // Get the name of the exchange from the request
+  const exchange = req.params.exchange;
+
+  // Initialize the fee variable
+  let fee;
+
+  // Determine which exchange's fee function to call based on the exchange name
+  switch (exchange) {
+    case "Coinbase":
+      fee = CoinbaseFee(amount);
+      break;
+    case "Gemini":
+      fee = GeminiFee(amount);
+      break;
+    case "Kucoin":
+      fee = KuCoinFee(amount, marketOrder);
+      break;
+    case "Binance":
+      fee = BinanceFee(amount, marketOrder);
+      break;
+    case "CryptoCom":
+      fee = CryptoComFee(amount, marketOrder);
+      break;
+    case "FTX":
+      fee = FTXFee(amount, marketOrder);
+      break;
+    default:
+      fee = null;
+      break;
+  }
+
+  // Return the fee in the response
+  res.json({ fee });
 });
 
-// Set up route to set new Gemini fee
-app.put('/exchange/gemini/:fee', (req, res) => {
-  // Get fee from URL parameter
-  const fee = req.params.fee;
-  // Set new Gemini fee
-  ExchangeHandler.setNewGemeniFee(fee);
-  // Send success message as response
-  res.send(`Successfully set Gemini fee to ${fee}`);
-});
-
-// Set up route to set new KuCoin fee
-app.put('/exchange/kucoin/:fee/:orderType', (req, res) => {
-  // Get fee and order type from URL parameters
-  const fee = req.params.fee;
-  const orderType = req.params.orderType;
-  // Set new KuCoin fee
-  ExchangeHandler.setNewKuCoinFee(fee, orderType);
-  // Send success message as response
-  res.send(`Successfully set KuCoin fee to ${fee} for ${orderType} orders`);
-});
-
-// Set up route to set new Binance fee
-app.put('/exchange/binance/:fee', (req, res) => {
-  // Get fee from URL parameter
-  const fee = req.params.fee;
-  // Set new Binance fee
-  ExchangeHandler.setNewBinanceFee(fee);
-  // Send success message as response
-  res.send(`Successfully set Binance fee to ${fee}`);
-});
-
-// Set up route to set new Crypto.com fee
-app.put('/exchange/cryptocurrency/:fee/:orderType', (req, res) => {
-  // Get fee and order type from URL parameters
-  const fee = req.params.fee;
-  const orderType = req.params.orderType;
-  // Set new Crypto.com fee
-  ExchangeHandler.setNewCryptoComFee(fee, orderType);
-  // Send success message as response
-  res.send(`Successfully set Crypto.com fee to ${fee} for ${orderType} orders`);
-});
-
-// Set up route to set new FTX fee
-// Set up route to set new FTX fee
-app.put('/exchange/ftx/:fee/:orderType', (req, res) => {
-  // Get fee and order type from URL parameters
-  const fee = req.params.fee;
-  const orderType = req.params.orderType;
-  // Set new FTX fee
-  ExchangeHandler.setNewFTXFee(fee, orderType);
-  // Send success message as response
-  res.send(`Successfully set FTX fee to ${fee} for ${orderType} orders`);
-});
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+module.exports = router;
